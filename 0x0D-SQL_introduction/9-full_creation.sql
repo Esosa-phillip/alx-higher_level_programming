368317a07cb0b6a5afd5c3a64984815e9d8ee39d
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-11-30 01:09:34 +0100

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
+CREATE IF NOT EXISTS second_table(
+    id INT
+    name VARCHAR()
+    score INT);
+INSERT INTO second_table(1, "John", 10);
+INSERT INTO second_table(2, "Alex", 3);
+INSERT INTO second_table(3, "Bob", 14);
+INSERT INTO second_table(4, "George", 8);
