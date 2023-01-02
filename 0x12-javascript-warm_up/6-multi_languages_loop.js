a485a62bf8f4f1249f4073276baafc24e16dbea4
Esosa Omoigui <esosa.phillip@yahoo.com>
2023-01-02 18:23:54 +0100

    Prints 3 lines using an array and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in msg) {
   console.log(msg[i]);
 }
