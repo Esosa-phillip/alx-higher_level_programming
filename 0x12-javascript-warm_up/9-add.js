02b1d347651940a4034518a5b80b9fdddcaec191
Esosa Omoigui <esosa.phillip@yahoo.com>
2023-01-02 19:47:29 +0100

    Prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,7 +5,7 @@ function add(a, b) {
   if (isNaN(a) || isNaN(b)) {
     return (NaN);
   } else {
-      return (parseInt(a) + parseInt(b));
+    return (parseInt(a) + parseInt(b));
   }
 }
 console.log(add(a, b));
