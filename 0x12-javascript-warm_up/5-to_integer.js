12664fb0d723165ce2441977af0f51ec58e8795b
Esosa Omoigui <esosa.phillip@yahoo.com>
2023-01-02 18:08:56 +0100

    Prints a number depending on argument passed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Args = process.argv[2];
-if (isNaN(Args)) {
+const args = process.argv[2];
+if (isNaN(args)) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + Args);
+  console.log('My Number: ' + args);
 }
