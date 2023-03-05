a96ba8bac9d64914e6d0a15330bb50ab416523af
Esosa Omoigui <esosa.phillip@yahoo.com>
2023-03-05 17:15:46 +0100

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" script that prints the value of the X-Request-Id
-    variable found in the header of the response.
-"""
-if __name__ == '__main__':
-    import requests
-    html = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(html.text.__class__))
-    print("\t- content: {}".format(html.text))
+""" print request id """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    ri = r.headers.get('X-Request-Id')
+    print(ri)
