b8e83628f0d6a77c868393bea6a3cbcafcd3a3f1
Esosa Omoigui <esosa.phillip@yahoo.com>
2023-03-06 14:17:37 +0100

    Script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const files = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(files, content, 'utf8', err => {
-    if (err) {
-        console.error(err);
-    }
+  if (err) {
+    console.error(err);
+  }
 });
